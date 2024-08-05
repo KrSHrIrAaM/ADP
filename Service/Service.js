140b5fba5eaf6517cc593be030a9bca483518e29
KrSHrIrAaM <150787811+KrSHrIrAaM@users.noreply.github.com>
2024-08-05 11:50:55 +0530

    Update Service.js

--- Service/Service.js
+++ Service/Service.js
@@ -6,6 +6,12 @@ import serviceImage3 from '../Assests/service3.png';
 import serviceImage4 from '../Assests/service4.png';
 import newsImage1 from '../Assests/5g.png';
 import newsImage2 from '../Assests/wifi.png';
+import greatImage from '../Assests/great.png';
+import SerVedio from '../Assests/true-5g.mp4';
+import cloudVedio from '../Assests/cloud.mp4';
+import arVedio from '../Assests/ar.mp4';
+import deepVedio from '../Assests/deep.mp4';
+import LightVedio from '../Assests/lightening.mp4';
 
 const Service = () => {
   return (
@@ -42,6 +48,32 @@ const Service = () => {
           <h2>Service 6</h2>
           <p>We deliver outstanding service 6 for all your projects.</p>
         </div>
+      </div>
+      <div className="globe-container">
+        <img src={greatImage} alt="Great Service" className="globe-image" />
+        <div className="globe-text">
+          <h2><strong>Everywhere In Globe</strong></h2>
+        </div>
+      </div>
+      <div className="service-5g">
+        <h1><strong>True 5G experience for enterprises</strong></h1>
+        <div className="video-container">
+          <video autoPlay loop>
+            <source src={arVedio} type="video/mp4" />
+          </video>
+          <video autoPlay loop>
+            <source src={cloudVedio} type="video/mp4" />
+          </video>
+          <video autoPlay loop>
+            <source src={deepVedio} type="video/mp4" />
+          </video>
+          <video  loop>
+            <source src={LightVedio} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div className='5gspecial'>
+        <h1><strong>What makes True 5G special?</strong></h1>
       </div>
     </div>
   );
